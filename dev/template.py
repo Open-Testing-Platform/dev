95017ff5bd64f76ee2a5789d50e32d14ef7e7744
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-05-19 17:57:49 +0700

    First commit

--- dev/template.py
+++ dev/template.py
@@ -1,4 +1,4 @@
-main_go_gw = '''
+main_go_gw = """
 package {SERVICE_NAME}
 
 import (
@@ -19,7 +19,7 @@ var (
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "{RPC_ENDPOINT}", "gRPC server endpoint")
 )
 
-func Run() error {
+func Run() error {{
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -27,15 +27,48 @@ func Run() error {
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
+	opts := []grpc.DialOption{{grpc.WithInsecure()}}
 	err := gw.RegisterSampleHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	if err != nil {
+	if err != nil {{
 		return err
-	}
-
-	glog.Infoln("Server is starting...")
+	}}
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	return http.ListenAndServe(":{HTTP_PORT}", mux)
-}
-'''
+}}
+"""
+
+dockerfile_go_gw = """
+FROM golang:1.16-alpine
+
+WORKDIR /app
+
+ADD . .
+
+RUN go build -o main
+
+FROM alpine
+
+WORKDIR /app
+
+COPY --from=0 /app/main .
+
+EXPOSE {HTTP_PORT}
+
+ENTRYPOINT [ "./main" ]
+
+CMD /bin/sh
+"""
+
+go_mod_gw = """
+module github.com/open-testing-platform/grpc-gateway
+
+go 1.16
+
+require (
+	github.com/golang/glog v0.0.0-20210429001901-424d2337a529
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.4.0
+	github.com/open-testing-platform/go-rpc v0.0.0-20210519094701-23c33ef2369c
+	google.golang.org/grpc v1.37.1
+)
+"""
--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
 
-
 setup(
-    name='dev',
-    version='1.0.0',
+    name="dev",
+    version="0.1.0",
+    py_modules=["dev"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dev = dev:app",
+        ],
+    },
 )
