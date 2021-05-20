9c8f78d0fa51551f7c4ce76bbc216018e908bc02
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-05-20 10:35:20 +0700

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="dev",
     version="0.1.0",
-    py_modules=["dev"],
+    packages=find_packages(),
     install_requires=[
         "Click",
     ],
